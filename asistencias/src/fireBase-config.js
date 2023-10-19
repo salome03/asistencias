d1a8897395590e698ef10ee90bd8119217b02107
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-19 00:07:06 -0300

    primer comentario

---
+++ asistencias/src/fireBase-config.js
@@ -0,0 +1,9 @@
+/*export default{
+    apiKey: "AIzaSyDl5caIr8ZhPwyc5P03sezBzYBiw9kPbfU",
+    authDomain: "mindfulcheckin-66ee1.firebaseapp.com",
+    projectId: "mindfulcheckin-66ee1",
+    storageBucket: "mindfulcheckin-66ee1.appspot.com",
+    messagingSenderId: "492943990487",
+    appId: "1:492943990487:web:dac8042812208f308f9a92",
+    measurementId: "G-2H96P6YRVS"
+  };*/
