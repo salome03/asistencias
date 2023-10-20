1d8f182101b459ed80a77440dcea0318cdd39960
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-19 21:35:19 -0300

    Importando configuracion de autenticacion de google

--- asistencias/src/Componentes/Login_G.js
+++ asistencias/src/Componentes/Login_G.js
@@ -2,3 +2,4 @@ import React from "react";
 import Google from './Img/Google.png';
 import './styles/Boton_G.css';
 import { auth } from "../fireBase-config";
+import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
