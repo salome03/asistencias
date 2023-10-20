2e9e1b00f604de2b337ea55ee140b97c0fdf655a
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-19 21:36:24 -0300

    Definiendo funcion

--- asistencias/src/Componentes/Login_G.js
+++ asistencias/src/Componentes/Login_G.js
@@ -3,3 +3,7 @@ import Google from './Img/Google.png';
 import './styles/Boton_G.css';
 import { auth } from "../fireBase-config";
 import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
+
+const BotonGoogle = () => {
+    
+}
