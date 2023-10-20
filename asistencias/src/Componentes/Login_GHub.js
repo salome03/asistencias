cd1a573af5434191c894f5ab7be816e869f2a2e8
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-19 22:46:49 -0300

    Importando autotenticacion

--- asistencias/src/Componentes/Login_GHub.js
+++ asistencias/src/Componentes/Login_GHub.js
@@ -2,3 +2,4 @@ import React from "react";
 import GitHub from './Img/GitHub.png';
 import './styles/Boton_GHub.css';
 import { auth } from "../fireBase-config";
+import { GithubAuthProvider, signInWithPopup } from "@firebase/auth";
