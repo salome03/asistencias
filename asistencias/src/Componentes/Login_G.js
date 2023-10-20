d0bcb9ea77a7242724ae8bf044602c1672cf3fad
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-19 21:50:00 -0300

    Envio de mensaje si no fue logeado correctamente

--- asistencias/src/Componentes/Login_G.js
+++ asistencias/src/Componentes/Login_G.js
@@ -12,7 +12,7 @@ const BotonGoogle = () => {
             alert("Logeado correctamente");
         })
         .catch((error) => {
-            
+            console.log(error);
         })
     }
 }
