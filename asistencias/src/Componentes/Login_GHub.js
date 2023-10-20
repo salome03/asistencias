7406ee46b96c08df4e9b6fd50136dd2e01b7e2b0
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-19 22:48:51 -0300

    Definiendo funcion

--- asistencias/src/Componentes/Login_GHub.js
+++ asistencias/src/Componentes/Login_GHub.js
@@ -3,3 +3,7 @@ import GitHub from './Img/GitHub.png';
 import './styles/Boton_GHub.css';
 import { auth } from "../fireBase-config";
 import { GithubAuthProvider, signInWithPopup } from "@firebase/auth";
+
+const BotonGitHub = () => {
+    
+}
