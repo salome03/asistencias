d713ee2abca6be17a1e32a823586f7d393391288
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-25 17:54:20 -0300

    Definiendo funcion

--- asistencias/src/Componentes/Login_A.js
+++ asistencias/src/Componentes/Login_A.js
@@ -2,3 +2,7 @@ import React from "react";
 import Anonimo from "./Img/Anonimo.png";
 import { auth } from "../fireBase-config";
 import { signInAnonymously } from "firebase/auth";
+
+const BotonAnonimo = () => {
+    
+}
