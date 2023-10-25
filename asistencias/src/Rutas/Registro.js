e961186e7b2e787eec08c893034f4df1f512eff3
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-25 19:55:16 -0300

    Creando condicion

--- asistencias/src/Rutas/Registro.js
+++ asistencias/src/Rutas/Registro.js
@@ -2,3 +2,7 @@ import React, { useState } from "react";
 import '../Componentes/Styles/Registro.css';
 import { auth } from "../fireBase-config";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
+
+const Registro = () => {
+    
+}
