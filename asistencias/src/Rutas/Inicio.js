30eacaf02306c81e06e355b4276b82ba7664a94a
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-25 18:37:53 -0300

    Importando login de git hub  al proyecto

--- asistencias/src/Rutas/Inicio.js
+++ asistencias/src/Rutas/Inicio.js
@@ -2,3 +2,4 @@ import React, { useState } from "react";
 import { auth } from "../fireBase-config";
 import { signInWithEmailAndPassword } from "@firebase/auth";
 import BotonGoogle from "../Componentes/Login_G";
+import BotonGitHub from "../Componentes/Login_GHub";
