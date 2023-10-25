49910034371cdef284d0bff1e5519793bdf86aca
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-25 19:13:32 -0300

    Envio de mensaje que se logeo correctamente

--- asistencias/src/Rutas/Inicio.js
+++ asistencias/src/Rutas/Inicio.js
@@ -14,6 +14,7 @@ const Login = () => {
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             console.log(userCredential);
+            alert("Logeado correctamente");
         })
     }
 }
