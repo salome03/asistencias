5e4ec1319c101b9eb74d5f3e024f97cb80d17148
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-25 18:37:05 -0300

    Importando login de google al proyecto

--- asistencias/src/Rutas/Inicio.js
+++ asistencias/src/Rutas/Inicio.js
@@ -1,3 +1,4 @@
 import React, { useState } from "react";
 import { auth } from "../fireBase-config";
 import { signInWithEmailAndPassword } from "@firebase/auth";
+import BotonGoogle from "../Componentes/Login_G";
