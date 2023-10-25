0fc5170907ff7762d47f5fe50a20e1aae3c8c9dd
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-25 20:15:03 -0300

    Definiendo validacion de la funcion

--- asistencias/src/Rutas/Registro.js
+++ asistencias/src/Rutas/Registro.js
@@ -8,8 +8,8 @@ const Registro = () => {
     const [email, setEmail] = useState('');
     const [usuario, setUsuario] = useState('');
     const [password, setPassword] = useState('');
-    
-    const Datos = (e) => {
 
+    const Datos = (e) => {
+        e.preventDefault();
     }
 }
