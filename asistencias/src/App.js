e9acebef1df388d853102e7afbce865bf35a9ee7
salome03 <103341897+salome03@users.noreply.github.com>
2023-10-26 16:58:38 -0300

    Importando navegacion

--- asistencias/src/App.js
+++ asistencias/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LogoApp from './Componentes/Logo_App';
 import BotonLogin from './Componentes/Login';
 import Registro from './Rutas/Registro';
+import { BrowserRouter, Link, Route, Routes, NavLink } from 'react-router-dom';
 
 function App() {
   return (
