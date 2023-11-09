286760376c18f014e0747ba197e600f3fc7a4fda
salome03 <103341897+salome03@users.noreply.github.com>
2023-11-09 15:54:12 -0300

    Importando carusel de estilos

--- asistencias/src/tsx/BarraL.tsx
+++ asistencias/src/tsx/BarraL.tsx
@@ -1,3 +1,5 @@
 import { FC, useState } from 'react';
 import "./styles.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel as ReactCarousel } from 'react-responsive-carousel';
+
